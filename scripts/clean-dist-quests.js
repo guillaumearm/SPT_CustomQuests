12fac1cf0342d7266448e5dae4744d3ff0975817
Guillaume ARM <trapcodien@gmail.com>
2022-07-01 15:48:19 +0200

    build: add clean:distquests script

---
+++ scripts/clean-dist-quests.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const DISABLED_PREFIX = '.disabled';
+const DIST_QUESTS_FOLDER = 'dist/quests';
+
+const main = async () => {
+  const cwd = process.cwd();
+  const questsPath = path.join(cwd, DIST_QUESTS_FOLDER);
+  
+  // could be directory too
+  const fileNames = fs.readdirSync(questsPath);
+
+  fileNames.forEach(fileName => {
+    if (!fileName.endsWith(DISABLED_PREFIX)) {
+      fs.rmSync(path.join(questsPath, fileName), { recursive: true, force: true });
+    }
+  });
+}
+
+main();
